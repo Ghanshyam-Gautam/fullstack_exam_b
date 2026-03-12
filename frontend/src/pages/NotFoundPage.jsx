d7299a8ac3879140a13620b5340ba5005c0a41f3
Ghanshyam-Gautam <Ghanshyam.gautam@metropolia.fi>
2026-03-12 17:18:31 +0200

    changes on notfound page

--- frontend/src/pages/NotFoundPage.jsx
+++ frontend/src/pages/NotFoundPage.jsx
@@ -4,7 +4,7 @@ const NotFound = () => {
   return (
     <div className="not-found">
       <h2>Sorry</h2>
-      <p>That page cannot be found</p>
+      <p> Error 404 ! That page cannot be found</p>
       <Link to="/">Back to the homepage...</Link>
     </div>
   );
